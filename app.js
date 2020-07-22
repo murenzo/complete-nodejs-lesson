d20dd2443cb864899a0b1d27fdc556b6ff019685
Banjoko Abdulazeez <seunbanjoko2005@yahoo.com>
2020-07-22 03:37:53 +0100

    Implemented a simple server using the http default module

---
+++ app.js
@@ -0,0 +1,42 @@
+const http = require("http");
+const fs = require("fs");
+
+const server = http.createServer((req, res) => {
+  const { url, method } = req;
+  if (url === "/") {
+    res.setHeader("Content-Type", "text/html");
+    res.write("<html>");
+    res.write("<head><title>Enter message</title></head>");
+    res.write(
+      "<body><form action='/message' method='POST'><input type='text' name='message'><button type='submit'>Send</button></form></body>",
+    );
+    res.write("</html>");
+    return res.end();
+  }
+
+  if (url === "/message" && method === "POST") {
+    const body = [];
+    req.on("data", (chunk) => {
+      body.push(chunk);
+    });
+    req.on("end", () => {
+      const parsedBody = Buffer.concat(body).toString();
+      const message = parsedBody.split("=")[1];
+      fs.writeFileSync("message.txt", message);
+    });
+    res.statusCode = 302;
+    res.setHeader("Location", "/");
+    return res.end();
+  }
+
+  res.setHeader("Content-Type", "text/html");
+  res.write("<html>");
+  res.write("<head><title>My first html response</title></head>");
+  res.write(
+    "<body><h2>Welcome to my first html response using node</h2></body>",
+  );
+  res.write("</html>");
+  res.end();
+});
+
+server.listen(3300);
