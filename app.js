20f725b3d925ac86882116e0c3f3563f8dfee09e
Banjoko Abdulazeez <seunbanjoko2005@yahoo.com>
2020-07-24 00:36:30 +0100

    Implemented serving static files & modified all inline css to importing the central css file

--- app.js
+++ app.js
@@ -1,7 +1,6 @@
 const path = require("path");
 
 const express = require("express");
-
 const bodyParser = require("body-parser");
 
 const app = express();
@@ -10,6 +9,7 @@ const adminRoutes = require("./routes/admin");
 const shopRoutes = require("./routes/shop");
 
 app.use(bodyParser.urlencoded({ extended: false }));
+app.use(express.static(path.join(__dirname, "public")));
 
 app.use("/admin", adminRoutes);
 app.use(shopRoutes);
