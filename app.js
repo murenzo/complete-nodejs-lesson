7f5e86f876b74c36ffedc6e3c8516baabefca52f
Banjoko Abdulazeez <seunbanjoko2005@yahoo.com>
2020-07-23 23:50:58 +0100

    Created a views folder to store html files, implemented a root directory util helper function

--- app.js
+++ app.js
@@ -1,3 +1,5 @@
+const path = require("path");
+
 const express = require("express");
 
 const bodyParser = require("body-parser");
@@ -14,6 +16,6 @@ app.use(shopRoutes);
 
 //Handles 404 request
 app.use((req, res, next) => {
-  res.status(404).send("<h2>Page not found</h2>");
+  res.status(404).sendFile(path.join(__dirname, "views", "404.html"));
 });
 app.listen(3500);
