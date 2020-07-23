03e4568f20188ffcf0f4403c5ea7e70a42840c5e
Banjoko Abdulazeez <seunbanjoko2005@yahoo.com>
2020-07-23 23:07:09 +0100

    seperated my routes into model based categories and imported them into the application entry

--- app.js
+++ app.js
@@ -4,22 +4,12 @@ const bodyParser = require("body-parser");
 
 const app = express();
 
+const adminRoutes = require("./routes/admin");
+const shopRoutes = require("./routes/shop");
+
 app.use(bodyParser.urlencoded({ extended: false }));
 
-app.use("/add-product", (req, res, next) => {
-  res.send(
-    "<form action='/product' method='POST'><input type='text' name='title'><button type='submit'>Add Product</button></form>"
-  );
-});
-
-app.post("/product", (req, res, next) => {
-  const { title } = req.body;
-  console.log(title);
-  res.redirect("/");
-});
-
-app.use("/", (req, res, next) => {
-  res.send("<h1>Hello from express</h1>");
-});
+app.use(adminRoutes);
+app.use(shopRoutes);
 
 app.listen(3500);
