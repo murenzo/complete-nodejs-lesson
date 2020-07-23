8361c9e74ce4918c219d9f7ca46790d7c8dd46b7
Banjoko Abdulazeez <seunbanjoko2005@yahoo.com>
2020-07-23 22:55:51 +0100

    Installed body-parserand completed basic setup for my apis

--- app.js
+++ app.js
@@ -1,5 +1,25 @@
-const http = require("http");
+const express = require("express");
 
-const server = http.createServer();
+const bodyParser = require("body-parser");
 
-server.listen(3500);
+const app = express();
+
+app.use(bodyParser.urlencoded({ extended: false }));
+
+app.use("/add-product", (req, res, next) => {
+  res.send(
+    "<form action='/product' method='POST'><input type='text' name='title'><button type='submit'>Add Product</button></form>"
+  );
+});
+
+app.post("/product", (req, res, next) => {
+  const { title } = req.body;
+  console.log(title);
+  res.redirect("/");
+});
+
+app.use("/", (req, res, next) => {
+  res.send("<h1>Hello from express</h1>");
+});
+
+app.listen(3500);
