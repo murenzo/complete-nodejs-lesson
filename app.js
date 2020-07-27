91cc382d943c1ba9f768ef73ad970935fb74303d
Banjoko Abdulazeez <seunbanjoko2005@yahoo.com>
2020-07-27 19:04:05 +0100

    Installed and configured express-handlebars, modified all html pages to handlebars page

--- app.js
+++ app.js
@@ -2,8 +2,19 @@ const path = require("path");
 
 const express = require("express");
 const bodyParser = require("body-parser");
+const expressHandlebars = require("express-handlebars");
 
 const app = express();
+
+app.engine(
+  "hbs",
+  expressHandlebars({
+    extname: "hbs",
+    defaultLayout: false,
+  })
+);
+app.set("view engine", "hbs");
+app.set("views", "views");
 
 const adminData = require("./routes/admin");
 const shopRoutes = require("./routes/shop");
@@ -16,6 +27,6 @@ app.use(shopRoutes);
 
 //Handles 404 request
 app.use((req, res, next) => {
-  res.status(404).sendFile(path.join(__dirname, "views", "404.html"));
+  res.status(404).render("404", { pageTitle: "Page not found" });
 });
 app.listen(3500);
