28a00e626af63f82de291d3717f48acc46439d35
Banjoko Abdulazeez <seunbanjoko2005@yahoo.com>
2020-07-31 07:01:54 +0100

    Finish modifying code architecture

--- app.js
+++ app.js
@@ -3,6 +3,8 @@ const path = require("path");
 const express = require("express");
 const bodyParser = require("body-parser");
 
+const errorController = require("./controllers/error");
+
 const app = express();
 
 app.set("view engine", "ejs");
@@ -18,7 +20,5 @@ app.use("/admin", adminRoutes);
 app.use(shopRoutes);
 
 //Handles 404 request
-app.use((req, res, next) => {
-  res.status(404).render("404", { pageTitle: "Page not found", path: "" });
-});
+app.use(errorController.getPageNotFound);
 app.listen(3500);
